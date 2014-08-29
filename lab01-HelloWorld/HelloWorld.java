6c7d3708669750ae51e5131cc76e577ae7a79845
rblumin <rsb317@lehigh.edu>
2014-08-29 14:23:47 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//Roger Blumin
+//Demo 0
+//Hello World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java Helloworld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+  public static void main(String[] args){
+
+//print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#  Hello World 2014!!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+      
+      
+  }
+}
