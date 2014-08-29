74b6b96409e05c33620d2ec108ca0acfe9075bc5
rblumin <rsb317@lehigh.edu>
2014-08-29 17:50:58 +0000

    homework1

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Roger Blumin
+//HW 1
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args){
+
+//print the welcome message with signature and autobiographical message
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-R--S--B--3--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("I have one younger brother named Philip.");
+    
+    
+  }    
+ 
+  
+}
